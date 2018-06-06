1890262e2976b18b5ea04e161a891c0379b64a8d
David Young <davidy@funkypenguin.co.nz>
2018-06-06 13:42:09 +1200

    Merged a bunch of previously-local changes

--- home-assistant/README.md
+++
@@ -1,3 +0,0 @@
-* 4/8/2017
-
-Couldn't get this stack to fly. The oauth_proxies seem to be trying go to DNS lookups for their upstream, instead of using service discovery in docker.
--- home-assistant/guest_wifi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Add eth1
-/root/docker/pipework/pipework br3 -i eth1 iot_homeassistant_1 192.168.30.6/24 54:52:00:30:00:06
-/root/docker/pipework/pipework route iot_homeassistant_1 add 224.0.0.50 dev eth1
