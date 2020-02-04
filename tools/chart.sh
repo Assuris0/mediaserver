cc512aa5928813125fc2713e9acb688a371d5d3a
Thomas Hobson <git@hexf.me>
2020-02-04 21:53:59 +1300

    Add lint command

--- tools/chart.sh
+++ tools/chart.sh
@@ -11,6 +11,10 @@ if [ "$1" = "create" ]; then
     ./tools/mkchart.sh $2
 fi
 
+if [ "$1" = "lint" ]; then
+    helm lint ./charts/$2/
+fi
+
 #Now I would try make something fancy, but I want to get onto writing some jucy recipies
 
 popd > /dev/null
