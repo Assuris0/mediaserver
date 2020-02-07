43a6cfcbe3bd79671ee00ff55849e57903acc396
Thomas Hobson <git@hexf.me>
2020-02-07 19:17:24 +1300

    Tool Set - Add Prepush git command

--- tools/chart.sh
+++ tools/chart.sh
@@ -6,6 +6,7 @@ Usage: $(basename $0) [command] [chart]
         create  -   Create a new chart with the name of [chart]
         lint    -   Lint [chart] with chart-testing
         update  -   Update the dependencies on [chart]
+        prepush -   Updates readme, updates dependencies and lints chart (The pre-push package!)
 
     Dependencies:
         https://github.com/helm/chart-testing
@@ -16,6 +17,24 @@ Usage: $(basename $0) [command] [chart]
 EOF
 }
 
+cmd_build(){
+    python tools/mkreadme.py tools/README.MD.TEMPLATE $CHART/README.MD $CHART/Chart.yaml $CHART/values.yaml
+}
+
+cmd_create(){
+    cd charts
+    helm create $2
+    cd ..
+}
+
+cmd_lint(){
+    ct lint --charts $CHART --config .ci/ct-config.yaml
+}
+
+cmd_update(){
+    helm dep update $CHART
+}
+
 pushd . > /dev/null
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../"
 
@@ -23,21 +42,24 @@ CHART=charts/$2
 
 case "$1" in 
     "build")
-        python tools/mkreadme.py tools/README.MD.TEMPLATE $CHART/README.MD $CHART/Chart.yaml $CHART/values.yaml
+        cmd_build
         ;;
 
     "create")
-        cd charts
-        helm create $2
-        cd ..
+        cmd_create
         ;;
 
     "lint")
-        ct lint --charts $CHART
+        cmd_lint
         ;;
 
     "update")
-        helm dep update $CHART
+        cmd_update
+        ;;
+    "prepush")
+        cmd_build
+        cmd_update
+        cmd_lint
         ;;
     *)
         show_help
