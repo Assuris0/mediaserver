12e221bf1df8f79069a997afc137f4bb493c6ccd
Thomas Hobson <git@hexf.me>
2020-02-07 17:19:17 +1300

    Merge tooling into a single file

--- tools/buildchart.sh
+++
@@ -1,2 +0,0 @@
-#This assumes root directory
-python tools/mkreadme.py tools/README.MD.TEMPLATE charts/$1/README.MD charts/$1/Chart.yaml charts/$1/values.yaml
--- tools/chart.sh
+++ tools/chart.sh
@@ -1,18 +1,38 @@
+clr(){
+    echo -e "\033[$1m"
+}
+
+
 pushd . > /dev/null
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../"
 
-echo "Usage: chart {build,create} [chartname]"
+echo "Usage: chart {build,create,lint} [chartname]"
+
+CHART=charts/$2
+
+# dependencies
+# gh: helm/helm
+# gh: helm/chart-tools
+# $ pip install yamale yamllint ruamel.yaml pystache
+
+
+
 
 if [ "$1" = "build" ]; then
-    ./tools/buildchart.sh $2
+    python tools/mkreadme.py tools/README.MD.TEMPLATE $CHART/README.MD $CHART/Chart.yaml $CHART/values.yaml
 fi
 
 if [ "$1" = "create" ]; then
-    ./tools/mkchart.sh $2
+    cd charts
+    helm create $2
+    cd ..
 fi
 
 if [ "$1" = "lint" ]; then
-    helm lint ./charts/$2/
+    echo -e "`clr '0;94'`helm lint`clr '0'`"
+    helm lint $CHART
+    echo -e "`clr '0;94'`ct lint`clr '0'`"
+    ct lint --charts $CHART
 fi
 
 #Now I would try make something fancy, but I want to get onto writing some jucy recipies
