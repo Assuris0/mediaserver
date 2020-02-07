277094289c7dfff250ceac690c2183e37c2d52ac
Thomas Hobson <git@hexf.me>
2020-02-07 18:03:56 +1300

    Tooling - Add Update command +move to switch

--- tools/chart.sh
+++ tools/chart.sh
@@ -1,32 +1,46 @@
+show_help (){
+    cat <<EOF 
+Usage: $(basename $0) [command] [chart]
+    Commands:
+        build   -   Create the README File for the [chart]
+        create  -   Create a new chart with the name of [chart]
+        lint    -   Lint [chart] with chart-testing
+        update  -   Update the dependencies on [chart]
+
+    Dependencies:
+        https://github.com/helm/chart-testing
+
+        $ gofish install helm
+        $ pip install yamale yamllint ruamel.yaml pystache
+
+EOF
+}
+
 pushd . > /dev/null
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../"
 
-echo "Usage: chart {build,create,lint} [chartname]"
-
 CHART=charts/$2
 
-# dependencies
-# gh: helm/helm
-# gh: helm/chart-tools
-# $ pip install yamale yamllint ruamel.yaml pystache
+case "$1" in 
+    "build")
+        python tools/mkreadme.py tools/README.MD.TEMPLATE $CHART/README.MD $CHART/Chart.yaml $CHART/values.yaml
+        ;;
 
+    "create")
+        cd charts
+        helm create $2
+        cd ..
+        ;;
 
+    "lint")
+        ct lint --charts $CHART
+        ;;
 
-
-if [ "$1" = "build" ]; then
-    python tools/mkreadme.py tools/README.MD.TEMPLATE $CHART/README.MD $CHART/Chart.yaml $CHART/values.yaml
-fi
-
-if [ "$1" = "create" ]; then
-    cd charts
-    helm create $2
-    cd ..
-fi
-
-if [ "$1" = "lint" ]; then
-    ct lint --charts $CHART --debug
-fi
-
-#Now I would try make something fancy, but I want to get onto writing some jucy recipies
+    "update")
+        helm dep update $CHART
+        ;;
+    *)
+        show_help
+esac
 
 popd > /dev/null
